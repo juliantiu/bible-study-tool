9cd414f0023e130bdcf96523a9f49f94fc6dad6f
Julian Tiu <jtiu.code@gmail.com>
2024-08-24 00:53:20 -0500

    added BibleVersionMetadata entity and corresponding config file

---
+++ BibleStudyTool.Core/Entities/BibleVersionMetadata/BibleVersionMetadata.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibleStudyTool.Core.Entities.BibleVersionMetadata
+{
+    public class BibleVersionMetadata
+    {
+        public string Language { get; private set; }
+        public string LanguageEndonym { get; private set; }
+        public string LanguageExonym { get; private set; }
+        public string VersionAbbreviation { get; private set; }
+        public string VersionFullName { get; private set; }
+
+        public Dictionary
+            <string, 
+                (
+                string BookKey,
+                string BookName,
+                string BookAbbreviation,
+                string Testament,
+                string Section,
+                string Subsection,
+                string ChapterCount,
+                string VerseCount
+                )>
+            BibleBookDefinitions { get; private set; }
+
+        public BibleVersionMetadata
+            (string language,
+            string languageEndonym,
+            string languageExonym,
+            string versionAbbreviation,
+            string versionFullName
+            )
+        {
+            Language = language;
+            LanguageEndonym = languageEndonym;
+            LanguageExonym = languageExonym;
+            VersionAbbreviation = versionAbbreviation;
+            VersionFullName = versionFullName;
+            BibleBookDefinitions =
+                new Dictionary
+                    <string,
+                    (
+                        string BookKey,
+                        string BookName,
+                        string BookAbbreviation,
+                        string Testament,
+                        string Section,
+                        string Subsection,
+                        string ChapterCount,
+                        string VerseCount
+                    )>();
+        }
+    }
+}
